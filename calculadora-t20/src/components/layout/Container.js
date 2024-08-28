633432e4ec2880a5d1445c0c5dee4b918685e63f
Víctor Duplat Tigre <98099656+tigrevictorduplat@users.noreply.github.com>
2024-08-28 10:09:28 -0300

    Update Container.js

--- calculadora-t20/src/components/layout/Container.js
+++ calculadora-t20/src/components/layout/Container.js
@@ -7,7 +7,7 @@ function Container (props) {
     return  <div className={`${styles.container} ${styles[props.customClass]}`} >{props.children}</div>
 }
 
-Container.propType = {
+Container.propTypes = {
     customClass: PropTypes.oneOf([
         ['start', 'column','min-height' ]
     ])
