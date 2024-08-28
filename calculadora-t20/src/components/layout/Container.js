2de33376ad43c688075b635eb83e965cbfda5502
Víctor Duplat Tigre <98099656+tigrevictorduplat@users.noreply.github.com>
2024-08-28 10:29:50 -0300

    Revert "Storybook - Stories"
    This reverts commit 25c2f30f4198d780cc21599f8a7cba2ca3d87460.

--- calculadora-t20/src/components/layout/Container.js
+++ calculadora-t20/src/components/layout/Container.js
@@ -4,15 +4,4 @@ function Container (props) {
     return  <div className={`${styles.container} ${styles[props.customClass]}`} >{props.children}</div>
 }
 
-Container.propType = {
-    customClass: PropTypes.oneOf([
-        ['start', 'column','min-height' ]
-    ])
-}
-
-Container.defaultProps = {
-    customClass : 'min-height'
-}
-
 export default Container
-
